77193f05faacf16391e8be4064f2fdd2e0b3ba11
arman8808 <armanal3066@gmail.com>
2026-03-08 19:24:04 +0530

    feat: Implement forgot password functionality with OTP generation and email sending for user, vendor, and admin roles.

--- controllers/forgot.controller.js
+++ controllers/forgot.controller.js
@@ -69,7 +69,7 @@ const authController = async (req, res) => {
 
     return res
       .status(200)
-      .json({ message: "OTP sent to the registered email and Whatsapp" });
+      .json({ message: "OTP sent to the registered email." });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Internal Server Error" });
